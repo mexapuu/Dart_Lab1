2217500df065ca9afbc5f2e3d706a6441d080458
Мухометова Олеся <omukhometova@mail.ru>
2026-09-07 11:38:02 +0300

    chaghe

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,5 +1,18 @@
 import 'package:lab1_dart/lab1_dart.dart' as lab1_dart;
 
 void main(List<String> arguments) {
-  print('Hello world: ${lab1_dart.calculate()}!');
+   String name = 'Артем';
+  int age = 20;
+  double height = 1.75;
+  bool isStudent = Мухометова Олеся;
+
+ print(name);
+ print(age);
+ print(height);
+ print(isStudent);
+
 }
+void main()
+print('Привет, $name! Тебе $age лет.');
+print('Через 5 лет тебе будет ${age = 5} лет.');
+print('Рост ${height} м, студент: $isStudent');
